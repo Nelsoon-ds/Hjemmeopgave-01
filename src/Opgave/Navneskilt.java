a1172efcc4d99e6d227bdf8cd7167a39ef4265d0
Nelsoon-ds <davidgaming108@gmail.com>
2025-08-28 11:48:49 +0200

    Lige ændret format

--- src/Opgave/Navneskilt.java
+++ src/Opgave/Navneskilt.java
@@ -3,18 +3,38 @@ package Opgave;
 public class Navneskilt {
     public static void main(String[] args) {
         printNavneskilt();
+        //printNavneskilt02();
+        //printAsterisk(name);
         // Should compile into this:
         // ********************
         //* David Søbæk Olsen*
         //*    daol@kea.dk   *
         //********************
+
     }
+
     public static void printNavneskilt() {
+      //  String fullName = " David Søbæk Olsen";
+        // printAsterisk(fullName);
+       // System.out.print('*');
+       //  System.out.print(fullName + "\t");
+      //   System.out.println("*");
+       // printAsterisk(fullName);
         System.out.println("*************************");
         System.out.print("*");
         System.out.println("\tDavid Søbæk Olsen" + "\t*");
         System.out.print("*");
-        System.out.println("\t\tdaol@kea.dk" + "\t\t*");
+        System.out.println("\tdaol0002@stud.ek.dk" + "\t*");
         System.out.print("*************************");
+    }
+    public static void printNavneskilt02() {
+        String text = "*************************\n*\n*";
+    }
+    public static void printAsterisk(String name){
+        for (int i = 0; i<=name.length(); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
     }
 }
